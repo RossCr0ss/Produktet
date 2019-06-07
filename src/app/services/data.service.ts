419825bffe64e559c20200901f1333c95a004ea6
Mihail Borzenko <borzenkomihail@yahoo.com>
2019-06-07 08:46:36 +0300

    Fixed errors

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,15 +1,17 @@
 import {Inject, Injectable} from '@angular/core';
-import {SiteConfigurationService} from './site-configuration.service';
+import {SiteConfigurationService} from '../shared/services/site-configuration.service';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import {GeneralData} from '../model/general-data.model';
+import {GeneralData} from '../shared/models/general-data.model';
 import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })export class DataService {
 
-  constructor(@Inject('BACKEND_API_URL') private backendApiUrl: string, private http: HttpClient, private siteConfigurationService: SiteConfigurationService) {
+  constructor(@Inject('BACKEND_API_URL') private backendApiUrl: string,
+    private http: HttpClient,
+    private siteConfigurationService: SiteConfigurationService) {
   }
 
   getData(url: string): Observable<any> {
