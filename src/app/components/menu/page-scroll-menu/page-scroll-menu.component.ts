862d9463d2a8bd2e1b3c6e46d31a5b00ae732c22
Anders Schmidt <as@aidgency.dk>
2019-08-13 11:53:40 +0200

    new frontend

--- src/app/components/menu/page-scroll-menu/page-scroll-menu.component.ts
+++ src/app/components/menu/page-scroll-menu/page-scroll-menu.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Inject} from '@angular/core';
 import {Menu} from "../../../shared/models/menu.model";
 import {Site} from "../../../shared/models/site.model";
 import {MenuService} from "../../../shared/services/menu.service";
 import {RouteService} from "../../../shared/services/route.service";
 import {SiteConfigurationService} from "../../../shared/services/site-configuration.service";
 import {Router} from "@angular/router";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-page-scroll-menu',
@@ -17,6 +18,7 @@ export class PageScrollMenuComponent implements OnInit {
   configuration: Site;
 
   constructor(
+    @Inject(DOCUMENT) private document: any,
     private menuService: MenuService,
     private routeService: RouteService,
     private siteConfiguration: SiteConfigurationService, private router: Router) {
@@ -31,6 +33,78 @@ export class PageScrollMenuComponent implements OnInit {
       this.routeService.setPageScrollDefaultRoute(this.siteConfiguration.configuration.content.name);
       this.router.navigateByUrl('/');
     });
+
+    const script = this.document.createElement('script');
+    script.type = 'text/javascript';
+    script.text = `
+    
+    setTimeout(function(){ 
+
+      let menu = document.querySelector('.sidebar-nav'),
+      menuButton = document.querySelector('.js-hamb'),
+      overlay = document.querySelector('.js-overlay'),
+      elevator = document.querySelector('.js-elevator');
+      
+      let MenuUI = {
+      
+      initialize() {
+          this.addListener();
+      },
+      
+      addListener() {
+          menuButton.addEventListener('click', this._listeners.menuSideToggle, false); // burger action
+          
+      },
+      
+      _listeners: {
+          
+          menuSideToggle() {
+              
+              if (!menu.classList.contains('active')) {
+                  
+                  //show menu
+                  MenuUI.action.menuSideOn();
+                  
+              } else {
+                  
+                  //hide menu
+                  MenuUI.action.menuSideOff();
+              }
+          }
+      },
+      
+      action: {
+          
+          menuSideOn() {
+              // show menu action
+              menu.classList.add('active');
+              menuButton.classList.add('active');
+              overlay.classList.add('active'); // show overlay
+              document.body.classList.add('blocked'); // prevent scroll of body
+              elevator.classList.add('active', 'visible');
+              
+          },
+          
+          menuSideOff() {
+              // hide menu action
+              menuButton.classList.remove('active');
+              menu.classList.remove('active');
+              elevator.classList.remove('active');
+              setTimeout(() => {
+                  overlay.classList.remove('active'); // hide overlay
+                  elevator.classList.remove('visible');
+                  document.body.classList.remove('blocked'); // prevent scroll of body
+              }, 500)
+          }
+      }
+      };
+      
+      MenuUI.initialize();
+   }, 200);
+
+    `;
+    this.document.head.appendChild(script);
+
   }
 
 }
