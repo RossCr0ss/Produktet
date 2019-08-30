1df980c16e6a166c82acfd0776c48c3f65c2b343
Anders Schmidt <as@aidgency.dk>
2019-08-30 13:44:14 +0200

    a little cleaning up

--- src/app/components/additional-content/video/video.component.ts
+++ src/app/components/additional-content/video/video.component.ts
@@ -1,9 +1,6 @@
 import { Component, Inject, OnInit, HostListener, Input, ViewChild, ElementRef } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { DOCUMENT } from "@angular/common";
 import { PlyrComponent } from 'ngx-plyr';
 import {Video} from "../../../shared/models/additional-contenet-model/video.model";
-import {SiteConfigurationService} from "../../../shared/services/site-configuration.service";
 
 @Component({
   selector: 'app-video',
@@ -14,20 +11,13 @@ export class VideoComponent implements OnInit {
 
   @ViewChild(PlyrComponent, { static: false }) plyr: PlyrComponent;
   plyrOptions: any;
-
   id: string;
-
-  imgPoster: string;
   content: Video;
 
   @ViewChild('player', { static: false }) player: ElementRef<HTMLMediaElement>;
 
-  constructor(
-    @Inject('BACKEND_API_URL') private backendApiUrl: string,
-    @Inject(DOCUMENT) private document: any,
-    private http: HttpClient, private siteConfigurationService: SiteConfigurationService) {
+  constructor() {
   }
-
 
   ngOnInit() {
     this.id = Math.random().toString(36).substring(7);
@@ -47,42 +37,6 @@ export class VideoComponent implements OnInit {
         loop: { active: this.content.isLoop }
       }
     }
-
-
-/*
-    this.http.get(`${this.backendApiUrl}menus`).subscribe((_: any[]) => {
-      _.forEach(element => {
-    this.siteConfigurationService.configuration.menus.forEach(element => {
-        let content = [];
-        if (element) {
-          content = element.content as Array<any>;
-        }
-        if (content) {
-          content.forEach(item => {
-            if (item.name === 'video') {
-              this.content = item;
-              if (!this.content.showControls) {
-                this.plyrOptions = {
-                  autoplay: this.content.isAutoplay,
-                  muted: this.content.isMuted,
-                  loop: { active: this.content.isLoop },
-                  controls: []
-                }
-              } else {
-                this.plyrOptions = {
-                  autoplay: this.content.isAutoplay,
-                  muted: this.content.isMuted,
-                  loop: { active: this.content.isLoop }
-                }
-              }
-
-            }
-          })
-        }
-      });
-    })
-
-    */
   }
 
 }
--- src/app/components/content/basic-content/basic-content.component.ts
+++ src/app/components/content/basic-content/basic-content.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {SiteConfigurationService} from '../../../shared/services/site-configuration.service';
 import {Site} from '../../..//shared/models/site.model';
-import {ComponentContent} from "../../../shared/models/component-content.model";
 
 
 @Component({
@@ -12,8 +11,6 @@ import {ComponentContent} from "../../../shared/models/component-content.model";
 export class BasicContentComponent implements OnInit {
 
   configuration: Site;
-  // Can be used to set content from the menu
-  additionalContent: ComponentContent;
 
   constructor(private siteConfigurationService: SiteConfigurationService) {
   }
