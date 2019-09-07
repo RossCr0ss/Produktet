66df24aa2d3c08a4b07c53245fcf005031fab071
Niranjan Joshi <niraj751018@gmail.com>
2019-09-07 22:35:51 +0545

    testimonials

--- src/app/components/additional-content/testimonial/testimonial.component.ts
+++ src/app/components/additional-content/testimonial/testimonial.component.ts
@@ -7,16 +7,13 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class TestimonialComponent implements OnInit {
   public iconColor = '#1de9c3';
-  public bgColor = 'transparent';
-  public textSize = '16px';
+  public bgColor = '#e7e7e7';
   public headingColor = '#fff';
   public textColor = '#302c2f';
-  public textBlockBg= '#fff';
+  public textBlockBg = '#fff';
   public iconSize = '35px';
-  public colCount = '3';
-  public alignCenter = 'center';
-  public alignLeft = 'left';
-  public borderBottom = '3px solid #881a7a';
+  public iconColor = '#b40095';
+  public borderBottom = '3px solid #b40095';
 
   constructor() {
   }
