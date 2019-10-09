fada39229bc1e615b8a5e6180d05f75091224a9e
sirDonger <ssrko98@gmail.com>
2019-10-09 13:52:24 +0300

    animation scroll fix

--- src/app/components/layout/basic-layout/basic-animation.ts
+++ src/app/components/layout/basic-layout/basic-animation.ts
@@ -43,7 +43,7 @@ export const slideAnimation =
     transition('* <=> *', [
       style({ position: 'relative' }),
       // During a transition, a new view is inserted directly after the old one and both elements appear on screen at the same time. To prevent this, apply additional styling to the host view, and to the removed and inserted child views. The host view must use relative positioning, and the child views must use absolute positioning. Adding styling to the views animates the containers in place, without the DOM moving things around.
-      query(':enter, :leave', [
+      query(':leave', [
         style({
           position: 'absolute',
           top: 0,
@@ -51,18 +51,25 @@ export const slideAnimation =
           width: '100%'
         })
       ], { optional: true }),
-      query(':enter', [
-        style({ left: '-100%'})
-      ],{ optional: true }),
-      query(':leave', animateChild(), { optional: true }),
-      group([
-        query(':leave', [
-          animate('2000ms ease-out', style({ left: '100%'}))
-        ], { optional: true }),
-        query(':enter', [
-          animate('2000ms ease-out', style({ left: '0%'}))
-        ], { optional: true })
-      ]),
-      query(':enter', animateChild(), { optional: true }),
+
+      query(':self', [
+        style({ 
+        position: 'absolute',
+        top: 0,
+        left: '-100%',
+        width: '100%' })
+      ], { optional: true }),
+
+      query(':leave', animate('000ms ease-out', style({ left: '100%'})), { optional: true }),
+      query(':self', animate('2000ms ease-out', style({ left: '0%'})), { optional: true }),
+
+      // query(':self', animateChild(), { optional: true }),
+      // group([
+      //   query(':self', [
+      //     animate('2000ms ease-out', style({ left: '0%'}))
+      //   ], { optional: true })
+      // ]),
+      
     ])
   ]);
+
