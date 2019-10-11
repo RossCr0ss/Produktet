9218220b53f98559f1074866093e9737f3508d79
sirDonger <ssrko98@gmail.com>
2019-10-11 10:36:50 +0300

    added new slider

--- src/app/components/additional-content/slider/slider.component.ts
+++ src/app/components/additional-content/slider/slider.component.ts
@@ -1,46 +1,68 @@
 import {AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
-import {NguCarouselConfig, NguCarousel} from '@ngu/carousel';
-import {Slider} from 'src/app/shared/models/additional-contenet-model/slider.model';
-
+// import {NguCarouselConfig, NguCarousel} from '@ngu/carousel';
+// import {Slider} from 'src/app/shared/models/additional-contenet-model/slider.model';
+import { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent implements AfterViewInit {
+export class SliderComponent implements OnInit {
+    images: string[];
+    config: SwiperOptions = {
+    speed: 1500,
+    autoplay: { delay: 4000, disableOnInteraction: false },
+    pagination: { el: '.swiper-pagination', clickable: true },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+        
+    };
 
-  BgColor: string;
-  FontColor: string;
+    constructor() {}
 
-  content: Slider;
+    ngOnInit() { this.images = [
+    'assets/images/yellow.jpg',
+    'assets/images/green.jpg',
+    'assets/images/blue.jpg'
+    ];}
 
-  withAnim = true;
-  mySlider: NguCarousel<any>;
+    }
+// export class SliderComponent implements AfterViewInit {
 
-  carouselConfig: NguCarouselConfig = {
-    grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
-    load: 3,
-    interval: {timing: 3000, initialDelay: 1000},
-    loop: false,
-    touch: true,
-    velocity: 0.2,
-  }
+//   BgColor: string;
+//   FontColor: string;
 
-  constructor(private cdr: ChangeDetectorRef) {
-  }
+//   content: Slider;
 
-  ngOnInit() {
-    this.FontColor = ' blue-grey-text text-lighten-5';
-    this.BgColor = ' blue-grey darken-3';
-  }
+//   withAnim = true;
+//   mySlider: NguCarousel<any>;
 
-  ngAfterViewInit() {
-    this.cdr.detectChanges();
-  }
+//   carouselConfig: NguCarouselConfig = {
+//     grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
+//     load: 3,
+//     interval: {timing: 3000, initialDelay: 1000},
+//     loop: false,
+//     touch: true,
+//     velocity: 0.2,
+//   }
 
-  moveTo(slide) {
-    this.mySlider.moveTo(slide, !this.withAnim);
-  }
+//   constructor(private cdr: ChangeDetectorRef) {
+//   }
 
-}
+//   ngOnInit() {
+//     this.FontColor = ' blue-grey-text text-lighten-5';
+//     this.BgColor = ' blue-grey darken-3';
+//   }
+
+//   ngAfterViewInit() {
+//     this.cdr.detectChanges();
+//   }
+
+//   moveTo(slide) {
+//     this.mySlider.moveTo(slide, !this.withAnim);
+//   }
+
+// }
--- src/app/components/additional-content/slider/slider.module.ts
+++ src/app/components/additional-content/slider/slider.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SliderComponent } from './slider.component';
 import {NguCarouselModule} from "@ngu/carousel";
-import {FormsModule} from "@angular/forms";
+// import {FormsModule} from "@angular/forms";
+import {NgxUsefulSwiperModule} from "ngx-useful-swiper";
+// import { SwiperComponent } from './swiper/swiper.component'
 
 @NgModule({
   imports: [
     CommonModule,
-    NguCarouselModule
+    NguCarouselModule,
+    NgxUsefulSwiperModule
   ],
   declarations: [SliderComponent],
   exports: [SliderComponent],
